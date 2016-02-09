b711b2b66fbe9f39ee9e242c021bf06d7c91ccd1
Wei Wang <shadowwalker1117@hotmail.com>
2016-02-09 16:25:33 -0500

    test get a small cube

--- code/scripts/getData.py
+++ code/scripts/getData.py
@@ -22,21 +22,27 @@ def main():
     print 'ndio version =',ndio.version
 
     metaData = getMetaData(TOKEN)
-    #pprint.pprint(metaData,indent=4)
+    # pprint.pprint(metaData,indent=4)
     imagesSizes = metaData['dataset']['imagesize']
     offset = metaData['dataset']['offset']
 
     n = u'5' # resolution
     print "ndio get cutout ... TOKEN=%s CHANNEL=%s RESOLUTION=%s"%(TOKEN,CHANNEL,n)
+    #data = nd.get_cutout(TOKEN,CHANNEL,
+    #                    imagesSizes[n][0]/2-50,imagesSizes[n][0]/2+50,
+    #                    imagesSizes[n][1]/2-50,imagesSizes[n][1]/2+50,
+    #                    imagesSizes[n][2]/2-50,imagesSizes[n][2]/2+50,
+    #                    resolution=int(n))
     data = nd.get_cutout(TOKEN,CHANNEL,
-                        offset[n][0],imagesSizes[n][0],
-                        offset[n][1],imagesSizes[n][1],
-                        offset[n][2],imagesSizes[n][2],
+                        0,100,
+                        0,100,
+                        0,100,
                         resolution=int(n))
+    pprint.pprint(data,indent=2)
     outFile = DATAPATH+'%s_%s_%s.csv'%(TOKEN,CHANNEL,n)
     np.savetxt(outFile,
                 data,
-                delimiter=',')
+                delimiter=',',fmt='%u')
     print "... Finished ... OUTPUT=%s"%(outFile)
 
 if __name__ == '__main__':
