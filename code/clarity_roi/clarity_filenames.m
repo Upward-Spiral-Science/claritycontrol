95f701ff79b8fc95e9607e2c43efae2917501691
shadowwalker <shadowwalker1117@hotmail.com>
2016-04-12 00:15:20 -0400

    add clarity_roi analysis codes

---
+++ code/clarity_roi/clarity_filenames.m
@@ -0,0 +1,21 @@
+function [mouse_fnames] = clarity_filenames()
+%% Fear
+% mouse_fnames{1} = './dataMatfile/fear187.mat';
+% mouse_fnames{2} = './dataMatfile/fear188.mat';
+mouse_fnames{1} = './dataMatfile/fear197.mat';
+mouse_fnames{2} = './dataMatfile/fear199.mat'; 
+mouse_fnames{3} = './dataMatfile/fear200.mat';
+%% Control 1 
+mouse_fnames{4} = './dataMatfile/control181.mat';
+mouse_fnames{5} = './dataMatfile/control182.mat';
+mouse_fnames{6} = './dataMatfile/control189.mat';
+% Control 2 Home Cage
+mouse_fnames{7} = './dataMatfile/control239.mat';
+mouse_fnames{8} = './dataMatfile/control258.mat';
+%% Cocaine 
+mouse_fnames{9} = './dataMatfile/cocaine173.mat';
+mouse_fnames{10} = './dataMatfile/cocaine174.mat';
+mouse_fnames{11} = './dataMatfile/cocaine175.mat';
+mouse_fnames{12} = './dataMatfile/cocaine177.mat';
+mouse_fnames{13} = './dataMatfile/cocaine178.mat';
+end 
